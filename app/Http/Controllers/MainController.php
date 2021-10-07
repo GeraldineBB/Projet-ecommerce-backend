140aed3414888e3471bdd26348560b29dcc3a043
GeraldineBB <geraldine-bornet@hotmail.fr>
2021-10-07 11:35:39 +0200

    structure back

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Models\Brand;
+use App\Models\Type;
+use App\Models\Category;
+
+use Illuminate\Http\Request;
+
+
+class MainController extends Controller
+{
+}
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,62 @@
 |
 */
 
+/**
+ * Route qui va permettre d'afficher tous les produits sur la home (avec selection date ajout)
+ */
+
 $router->get(
     '/',
     [
         'uses' => 'MainController@home',
         'as'   => 'main-home'
+    ]
+);
+
+/**
+ * Route qui permet d'afficher les produits sur "tous les produits"
+ */
+
+$router->get(
+    '/products',
+    [
+        'uses' => 'ProductController@list',
+        'as'   => 'product-list'
+    ]
+);
+
+/**
+ * Route permettant d'afficher un produit
+ */
+
+$router->get(
+    '/products/{id}',
+    [
+        'uses' => 'ProductController@item',
+        'as'   => 'product-item'
+    ]
+);
+
+/**
+ * Route permettant d'afficher en fonction de la catégorie
+ */
+
+$router->get(
+    '/products/category/{id}',
+    [
+        'uses' => 'ProductController@listCategory',
+        'as'   => 'product-listCategory'
+    ]
+);
+
+/**
+ * Route permettant d'afficher en fonction de la marque
+ */
+
+$router->get(
+    '/products/brand/{id}',
+    [
+        'uses' => 'ProductController@listBrand',
+        'as'   => 'product-listBrand'
     ]
 );
