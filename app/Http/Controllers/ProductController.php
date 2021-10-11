edac4ca1d958b4f74ca07bf00310232b5c69e9ab
GeraldineBB <geraldine-bornet@hotmail.fr>
2021-10-11 17:37:56 +0200

    lumen, controller, models

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,4 +12,28 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    /**
+     * URL : /products
+     *
+     * Méthode permettant la récupération de toutes les tâches
+     * au format JSON
+     *
+     * @return void
+     */
+    public function list()
+    {
+        // On récupère toutes les entrées de la table 'product'
+        // SELECT * FROM products;
+        // SELECT * FROM products;
+
+        // https://laravel.com/docs/6.x/eloquent#retrieving-models
+        $productsList = Product::all();
+
+
+        // en utilisant lumen notre API s'attend à avoir une réponse
+        // pour avoir une réponse on utilise la fonction response
+        // la réponse est vide : il faut lui indiquer ce qu'on veut mettre à l'intérieur
+        // on appelle la méthode json : cette requete vide tu vas la transformer en format json, et tu vas dire dans ta réponse que c'est du json
+        return response()->json($productsList);
+    }
 }
