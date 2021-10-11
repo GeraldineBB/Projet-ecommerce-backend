b71715b3c5068b4f047c3431f855d3d7cfd70e48
GeraldineBB <geraldine-bornet@hotmail.fr>
2021-10-11 19:52:36 +0200

    controller

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -24,16 +24,16 @@ class MainController extends Controller
     public function home()
     {
         // On récupère toutes les entrées de la table 'product'
-        // SELECT * FROM products;
-        // https://laravel.com/docs/6.x/eloquent#retrieving-models
         $productsList = Product::all();
 
 
         // en utilisant lumen notre API s'attend à avoir une réponse
         // pour avoir une réponse on utilise la fonction response
         // la réponse est vide : il faut lui indiquer ce qu'on veut mettre à l'intérieur
-        // on appelle la méthode json : cette requete vide tu vas la transformer en format json, et tu vas dire dans ta réponse que c'est du json
         return response()->json($productsList);
+
+        echo "main controller home";
+
     }
 
 
